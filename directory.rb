0e14aa1baee98870a31ad4ebab69f2c04746a6e4
Terry <terry@terrytilley.com>
2016-06-20 15:39:14 +0100

    Only print students with specific letter

--- directory.rb
+++ directory.rb
@@ -32,8 +32,10 @@ def print_header
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, num|
+    if student[:name].each_char.first == "M"
+      puts "#{num+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
