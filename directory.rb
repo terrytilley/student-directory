02f8722c04658267301919f33910fc24ae20b556
Terry <terry@terrytilley.com>
2016-06-20 11:15:53 +0100

    put student list to directory.rb

---
+++ directory.rb
@@ -0,0 +1,14 @@
+puts "-" * 50
+puts "The students of Villians Academy"
+puts "-" * 50
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffery Baratheon"
+puts "Norman Bates"
