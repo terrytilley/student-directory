3793e2687d34d1b20f3fa76d6d38af6babbdaca0
Terry <terry@terrytilley.com>
2016-06-20 11:49:26 +0100

    Added students into an array

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
-student_count = 11
-# First we print the list of students
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffery Baratheon",
+  "Norman Bates"
+]
+
 puts "The students of Villians Academy"
 puts "-" * 50
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffery Baratheon"
-puts "Norman Bates"
-# Finally, we print the total number of students
-puts "Overall, we have #{student_count} great students"
+students.each do |student|
+  puts student
+end
+
+puts "Overall, we have #{students.count} great students"
