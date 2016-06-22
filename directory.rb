ce74ad41368c18e15380016f918ccfc305e4a263
Terry <terry@terrytilley.com>
2016-06-22 10:04:57 +0100

    Added interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,24 @@
+def interactive_menu
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   students = []
   puts "** To finish, just hit return twice **"
@@ -67,7 +88,4 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
